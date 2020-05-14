ea699a5ebbfed415f99222c626e772ea9bd8bd1f
aeroman256 <zakharov.dmitry.92@yandex.ru>
2020-05-14 12:55:07 +0300

    124 завершен. react добавлен для рендера

--- src/index.js
+++ src/index.js
@@ -1,46 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
 import {createStore, bindActionCreators} from 'redux'
 import reducer from './reducer'
 import * as actions from './actions'
+import Counter from './counter'
 
 
 const store = createStore(reducer)
 const { dispatch } = store
-
-
 const { inc, dec, rnd } = bindActionCreators( actions, dispatch)
 
-
-
-//custom bindActionCreator
-
-// const bindActionCreator = (creator, dispatch) => (...args) => {
-//   dispatch(creator(...args))
-// }
-
-// const incDispatch = () => bindActionCreator(inc, dispatch)
-// const decDispatch = () => bindActionCreator(dec, dispatch)
-// const rndDispatch = () => bindActionCreator(rnd, dispatch)
-
-document
-  .getElementById("inc")
-  .addEventListener("click", inc)
-
-document
-  .getElementById("dec")
-  .addEventListener("click", dec)
-
-document
-  .getElementById("rnd")
-  .addEventListener("click", () => {
-    const payload = Math.floor(Math.random() * 10)
-    rnd(payload)
-  })
-
 const update = () => {
-  document
-    .getElementById("counter")
-    .innerHTML = store.getState()
+  ReactDOM.render(
+    <Counter 
+      counter={store.getState()}
+      
+      dec={dec}
+      inc={inc}
+      rnd={() => {
+        const value = Math.floor(Math.random() * 100)
+        rnd(value)
+      }}
+    />, document.getElementById("root"))
 }
 
+update()
+
 store.subscribe(update)
+
+
 
